f6fd95ca2d192d9c51a6a8dcb24c3d15aab54e03
rabiaislek <rabiaozbey96@gmail.com>
2022-01-14 15:33:47 +0300

    Order sayfasi

--- client/src/pages/Admin/Orders/index.js
+++ client/src/pages/Admin/Orders/index.js
@@ -1,9 +1,51 @@
 import React from 'react'
 
+import {useQery} from "react-query"
+import {fetchOrders} from "../../../api"
+
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, Text} from "@chakra-ui/react"
+
 function Orders() {
+    const {isLoading, isError, data, error} = useQery(
+        "admin:orders",
+        fetchOrders
+    )
+
+      if (isLoading) {
+          return <div>Loading...</div>
+      }
+
+      if (isError) {
+          return <div>Error {error.message}</div>
+      }
+
     return (
         <div>
-            Admin Orders
+            
+        <Text fontSize="2xl" p={5}>
+            Orders
+        </Text>
+
+        <Table variant="simple">
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+                <Tr>
+                    <Th>User</Th>
+                    <Th>Address</Th>
+                    <Th isNumeric>Items</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {data.map((item) => (
+                    <Tr key={item._id}>
+                        <Td>{item.user.email}</Td>
+                        <Td>{item.adress}</Td>
+                        <Td isNumeric>{item.items.length}</Td>
+                    </Tr>
+                ))}
+            </Tbody>
+        </Table>
+
         </div>
     )
 }
